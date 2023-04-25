2ca22030bef63e16c31a77f99e1d373a476ce8f2
Cong Nguyen <cong.nguyen@au1.ibm.com>
2023-04-25 22:41:33 +1000

    deploying to github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://one-touch-provisioning.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'one-touch-provisioning', // Usually your GitHub org/user name.
-  projectName: 'otp-gitops', // Usually your repo name.
+  projectName: 'one-touch-provisioning.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
